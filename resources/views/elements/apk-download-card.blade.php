8b0a298688caac6f49519491f318f355dd3cf3e0
Brandon Chong <phototix@hotmail.com>
2026-07-14 03:09:16 +0800

    Build 2026-07-14-BUILD-057

--- resources/views/elements/apk-download-card.blade.php
+++ resources/views/elements/apk-download-card.blade.php
@@ -2,7 +2,7 @@
     <div class="card-body text-center">
         <div class="mb-2">@include('elements.icon',['icon'=>'logo-android','variant'=>'large'])</div>
         <h6 class="font-weight-bold mb-2">{{ __('Android App') }}</h6>
-        <a href="{{ $apkDownloadUrl }}" class="btn btn-sm btn-primary btn-round d-inline-flex align-items-center">
+        <a href="{{ url('/apk/latest') }}" class="btn btn-sm btn-primary btn-round d-inline-flex align-items-center">
             @include('elements.icon',['icon'=>'download-outline','centered'=>false,'classes'=>'mr-1'])
             <span>{{ __('Download') }}</span>
         </a>
